dd7231738137a0dbeb67ae65934065a29efe1a49
christopherchoe <chchchoe@gmail.com>
2019-06-03 14:47:26 -0700

    first api blueprint

---
+++ application/server/api/api.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+API for job odyssey information
+"""
+from flask import Blueprint
+
+
+api_views = Blueprint('api_views', __name__, url_prefix='/api')
+# IMPORT OUR API FILES HERE
