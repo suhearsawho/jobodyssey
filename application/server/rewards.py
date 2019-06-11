677e22788a7b189351c9372e31bc1fddb7ccf3ff
Susan Su <susansu.software@gmail.com>
2019-06-11 11:48:36 -0700

    rewards file

---
+++ application/server/rewards.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, session, render_template, redirect, request, url_for
+from application.models.user import User
+from application.models import database
+import requests
+import os
+
+bp = Blueprint('rewards', __name__, url_prefix='/rewards', static_folder='../static/dist', template_folder='../static/templates')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def homepage():
+    if 'username' in session:
+        return render_template('user.html')
+    return render_template('index.html')
